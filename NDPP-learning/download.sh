b3788da03e4d5c7b352352001ba61e4cd2e79f5e
dqn1984 <2482854361@qq.com>
2025-02-25 23:00:14 +0800

    Delete NDPP-learning directory

--- NDPP-learning/download.sh
+++
@@ -1,3 +0,0 @@
-git clone -b scalable https://github.com/cgartrel/nonsymmetric-DPP-learning.git
-mv nonsymmetric-DPP-learning/data ./
-rm -rf nonsymmetric-DPP-learning
--- NDPP-learning/test.py
+++
@@ -1,7 +0,0 @@
-import random
-import numpy as np
-import torch
-
-if __name__ == "__main__":
-    model = torch.load("saved_models/qnli_sdim30_nsdim30_alpha0.1_VBC.torch")
-    print(model)
